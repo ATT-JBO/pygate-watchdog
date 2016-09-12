6da885acdb0e89038385c789a82d4e5ab9a0483a
Jan Bogaerts <jb@allthingstalk.com>
2016-09-12 17:11:48 +0200

    initial checkin

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pygate_watchdog',
+    version='1.0',
+    packages=['pygate_watchdog'],
+    url='www.allthingstalk.com',
+    license='',
+    author='Jan Bogaerts',
+    author_email='jb@allthingstalk.com',
+    description='pygate plugin: keep network connection alive'
+)
